26b1e079299e09dab2dfc4d2f87fca4599d6e696
Ali Yılmaz <111225614+Aliylmaz@users.noreply.github.com>
2022-08-27 14:52:38 +0300

    Sayı bulma
    0-dan-girilen-sayiya-kadar-olan-sayilardan-3-ve-4-e-tam-bolunen-sayilarin-ortalamasini bulma

---
+++ sayibulma.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class sayibulma {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int k, adet = 0, toplam = 0;
+
+        double ortalama;
+        System.out.println("Sayý girin :");
+        k = input.nextInt();
+        for (int i = 0; i <= k; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                System.out.println(i);
+                toplam += i;
+                adet++;
+
+
+            }
+        }
+        ortalama = toplam / (adet - 1);
+        System.out.println("SAYILARIN ORTALAMASI :" + ortalama);
+
+    }
+}
