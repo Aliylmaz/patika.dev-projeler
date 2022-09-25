1a18aafa700cbb4cc996a173bdb8cde19015e94c
Ali Yılmaz <111225614+Aliylmaz@users.noreply.github.com>
2022-09-25 12:38:50 +0300

    Create kuvvet.java

---
+++ DördünVeBeşinKuvvetleri/kuvvet.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DordunVeBesinKuvvetleri {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int a, b, c;
+        System.out.print("Sayı giriniz :");
+        a = input.nextInt();
+        for (b = 1; b <= a; b *= 4) {
+
+            System.out.println("4'ün kuvvetleri :" + b);
+
+
+        }
+        System.out.println("-------------------------");
+
+        for (c = 1; c <= a; c *= 5) {
+            System.out.println("5'in kuvvetleri :" + c);
+        }
+
+    }
+}
