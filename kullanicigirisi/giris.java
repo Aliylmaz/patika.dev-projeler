f3ccf2f63402d691170f02f020e6fd13532c5eca
Ali Yılmaz <111225614+Aliylmaz@users.noreply.github.com>
2022-09-25 12:49:52 +0300

    Update and rename kullanicigirisi.java to kullanicigirisi/giris.java

--- kullanicigirisi/giris.java
+++ kullanicigirisi/giris.java
@@ -13,23 +13,23 @@ public class kullanicigirisi {
         password = input.nextLine();
 
         if (username.equals("patika") && password.equals("java")) {
-            System.out.println(" Giriş yapıldı");
+            System.out.println(" GiriÅŸ yapÄ±ldÄ±");
         } else {
-            System.out.println("Şifreniz yanlıştır sıfırlamak isterseniz \"yes\" istemezseniz \"no\" seçin ");
+            System.out.println("Åifreniz yanlÄ±ÅŸtÄ±r sÄ±fÄ±rlamak isterseniz \"yes\" istemezseniz \"no\" seÃ§in ");
             sifreistegi = input.nextLine();
             if (sifreistegi.equals("no")) {
-                System.out.println("Sonlandı");
+                System.out.println("SonlandÄ±");
             } else if (sifreistegi.equals("yes")) {
                 System.out.print("Yeni sifrenizi girin :");
                 yenisifre=input.nextLine();
                 if (yenisifre.equals("java")){
-                    System.out.println("Yeni sifreyle eskisi aynı");
+                    System.out.println("Yeni sifreyle eskisi aynÄ±");
                 }else {
                     System.out.println("Sifre yenilendi!");
                 }
 
             }else {
-                System.out.println("Hatalı seçim seçtiniz");
+                System.out.println("HatalÄ± seÃ§im seÃ§tiniz");
             }
 
 
